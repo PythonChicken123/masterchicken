d291b50c924a1af53599d99c08392476a69edf44
NaboKabbo <101510622+PythonChicken123@users.noreply.github.com>
2023-11-25 12:51:57 +0000

    Create lumache.py

---
+++ lumache.py
@@ -0,0 +1,34 @@
+"""
+lumache.py
+"""
+
+import numpy
+import csv
+import pandas
+import functools
+import sys
+import os
+import importlib
+import subprocess
+import wheel
+
+class Lumache:
+    def __init__(self):
+        pass
+
+    def function1(self):
+        """
+        Description of function1
+        """
+        pass
+
+    def function2(self):
+        """
+        Description of function2
+        """
+        pass
+
+if __name__ == "__main__":
+    lm = Lumache()
+    lm.function1()
+    lm.function2()
