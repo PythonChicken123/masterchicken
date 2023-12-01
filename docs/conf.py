055c1d454cb286596e2c8241a8629abf5e33ae05
NaboKabbo <101510622+PythonChicken123@users.noreply.github.com>
2023-12-01 22:12:14 +0000

    Add docs/

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Masterchicken'
+copyright = '2023, Masterchicken developers'
+author = 'PythonChicken123'
+
+release = '0.1'
+version = '0.0.7'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'ext.headers',
+    'ext.boilerplate',
+    'ext.customversion',
+    'ext.edit_on_github'
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+pygments_style = 'sphinx'
+modindex_common_prefix = ['masterchicken']
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = "classic"
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+Welcome to Masterchicken's documentation!
+===================================
+
+**Masterchicken** (/lu'make/) is a Python library to speed up code and imports an optimized csv reader/writer, this CSV reader can read and print a 100,000 lined CSV file in a maxiumum of 7 seconds.
+
+Check out the :doc:`usage` section for further information, including
+how to :ref:`installation` the project.
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+   usage
+   api
