599dd96456b84e3b280057445450dd3a33959e6b
NaboKabbo <101510622+PythonChicken123@users.noreply.github.com>
2023-12-01 22:14:48 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = "classic"
+# html_theme = "classic"
+html_theme = ".themes/classic"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
