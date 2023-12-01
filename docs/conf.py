d7a2ff6741c1314e955daf7fbaa26105145120ec
NaboKabbo <101510622+PythonChicken123@users.noreply.github.com>
2023-12-01 19:57:21 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'Lumache'
 copyright = '2021, Graziella'
